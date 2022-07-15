56eb7886fe2a17ad5e6f45577016ea2b1261fe38
Freek <freek.de.sagher21@gmail.com>
2022-07-15 23:48:02 +0200

    prototype1

--- scripts/Dice.gd
+++ scripts/Dice.gd
@@ -2,6 +2,29 @@ extends RigidBody
 
 # TODO: movement veranderen wanneer ge met de muis + left click panned
 
+var mouse_down_start
 
-func apply_starter_force():
+export var torque_power = 5.0
+
+func _ready():
 	pass
+
+func _input(event):
+	if event.is_action_pressed("mouse_down"):
+		mouse_down_start = get_viewport().get_mouse_position()
+	if event.is_action_released("mouse_down"):
+		var dir: Vector2 = get_viewport().get_mouse_position() - mouse_down_start
+		dir = dir.normalized()
+		add_torque(Vector3(dir.y, 0, -dir.x) * torque_power);
+	if event.is_action_pressed("slowmo"):
+		Engine.time_scale = .5
+	if event.is_action_released("slowmo"):
+		Engine.time_scale = 1
+	
+	
+
+
+
+func _on_Hand_thrown():
+	add_force(Vector3(-150, 100, 0), Vector3(0,0,0))
+	add_torque(Vector3(randf(), randf(), randf()) * 10)
